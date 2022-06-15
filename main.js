216280fd0e089e30f190d41ba02052cd2473a952
Daniel Page <dwpage98@gmail.com>
2022-06-15 16:42:10 +1200

    Road markings and camera follow

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width/2,canvas.width*0.9);
-const car = new Car(100,100,30,50);
+const car = new Car(road.getLaneCenter(1),100,30,50);
 //car.draw(ctx);
 
 animate();
@@ -14,8 +14,12 @@ function animate() {
 
     canvas.height = window.innerHeight; // Refreshes drawing and updates height of canvas
     
+    ctx.save();
+    ctx.translate(0,-car.y);
+
     road.draw(ctx);
     car.draw(ctx);
 
+    ctx.restore();
     requestAnimationFrame(animate);
 }
