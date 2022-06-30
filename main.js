f0254aa6abc0653944f7e272eecf440745d80211
Daniel Page <dwpage98@gmail.com>
2022-06-30 13:11:57 +1200

    Sensors road border detection

--- main.js
+++ main.js
@@ -5,11 +5,12 @@ canvas.width = 200;
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width/2,canvas.width*0.9);
 const car = new Car(road.getLaneCenter(1),100,30,50);
+console.log(road.borders.length);
 
 animate();
 
 function animate() {
-    car.update();
+    car.update(road.borders);
 
     canvas.height = window.innerHeight; // Refreshes drawing and updates height of canvas
     
